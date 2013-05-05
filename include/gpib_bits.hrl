3e21e4ab8fac8db7cd6726ffbba8d4387f2c615d
Jared Kofron <jared.kofron@gmail.com>
2013-05-05 14:41:06 -0700

    added status byte register codes

--- include/gpib_bits.hrl
+++ include/gpib_bits.hrl
@@ -1,4 +1,18 @@
 -define(bit_set(Pos), (1 bsl Pos)).
+-define(byte(A,B), 
+	?bit_set(A) band ?bit_set(B)).
+-define(byte(A,B,C), 
+	?bit_set(A,B) band ?bit_set(C)).
+-define(byte(A,B,C,D),
+	?bit_set(A,B) band ?bit_set(C,D)).
+-define(byte(A,B,C,D,E),
+	?bit_set(A,B,C,D) band ?bit_set(E)).
+-define(byte(A,B,C,D,E,F),
+	?bit_set(A,B,C) band ?bit_set(D,E,F)).
+-define(byte(A,B,C,D,E,F,G),
+	?bit_set(A,B,C,D) band ?bit_set(E,F,G)).
+-define(byte(A,B,C,D,E,F,G,H),
+	?bit_set(A,B,C,D) band ?bit_set(E,F,G,H)).
 
 % Event Status Register definitions
 -define(op_compl, 0).
@@ -10,17 +24,23 @@
 -define(user_req, 6).
 -define(power_on, 7).
 -define(esr(Bit), ?bit_set(Bit)).
--define(esr(A,B), 
-	?bit_set(A) band ?bit_set(B)).
--define(esr(A,B,C), 
-	?bit_set(A,B) band ?bit_set(C)).
--define(esr(A,B,C,D), 
-	?bit_set(A,B) band ?bit_set(C,D)).
--define(esr(A,B,C,D,E), 
-	?bit_set(A,B,C,D) band ?bit_set(E)).
--define(esr(A,B,C,D,E,F), 
-	?bit_set(A,B,C) band ?bit_set(D,E,F)).
--define(esr(A,B,C,D,E,F,G), 
-	?bit_set(A,B,C,D) band ?bit_set(E,F,G)).
--define(esr(A,B,C,D,E,F,G,H),
-	?bit_set(A,B,C,D) band ?bit_set(E,F,G,H)).
+-define(esr(A,B), ?byte(A,B)).
+-define(esr(A,B,C), ?byte(A,B,C)).
+-define(esr(A,B,C,D), ?byte(A,B,C,D)).
+-define(esr(A,B,C,D,E), ?byte(A,B,C,D,E)).
+-define(esr(A,B,C,D,E,F), ?byte(A,B,C,D,E,F)).
+-define(esr(A,B,C,D,E,F,G), ?byte(A,B,C,D,E,F,G)).
+-define(esr(A,B,C,D,E,F,G,H), ?byte(A,B,C,D,E,F,G,H)).
+
+% Status Byte Register definitions
+-define(request_svc, 6).
+-define(event_summ, 5).
+-define(msg_avail, 4).
+-define(stb(Bit), ?bit_set(Bit)).
+-define(stb(A,B), ?byte(A,B)).
+-define(stb(A,B,C), ?byte(A,B,C)).
+-define(stb(A,B,C,D), ?byte(A,B,C,D)).
+-define(stb(A,B,C,D,E), ?byte(A,B,C,D,E)).
+-define(stb(A,B,C,D,E,F), ?byte(A,B,C,D,E,F)).
+-define(stb(A,B,C,D,E,F,G), ?byte(A,B,C,D,E,F,G)).
+-define(stb(A,B,C,D,E,F,G,H), ?byte(A,B,C,D,E,F,G,H)).
